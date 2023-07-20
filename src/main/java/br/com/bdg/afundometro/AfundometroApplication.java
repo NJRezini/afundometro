2a343bd0a1ab8fe3137494152e8e0a1ee24dc5e5
NJRezini <nathanrezini@gmail.com>
2023-07-20 14:45:27 -0300

    Adicionar estrutura inicial do projeto

---
+++ src/main/java/br/com/bdg/afundometro/AfundometroApplication.java
@@ -0,0 +1,13 @@
+package br.com.bdg.afundometro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfundometroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AfundometroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/bdg/afundometro/AfundometroApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bdg.afundometro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AfundometroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
